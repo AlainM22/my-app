c885a35ecf187828208902a87ac3b957ceeeccf9
Curso <Curso@IPARTEKAULA.COM>
2018-11-27 12:43:13 +0100

    Ejemplo flujo datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
 import { AlumnoComponent } from './alumno/alumno.component';
+import { FlujoInformacionComponent } from './flujo-informacion/flujo-informacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrutaComponent,
-    AlumnoComponent
+    AlumnoComponent,
+    FlujoInformacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
