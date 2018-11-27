c727d8a9bce9053d653af76b7940fce10e9c6032
Curso <Curso@IPARTEKAULA.COM>
2018-11-27 13:29:09 +0100

    Maquetación Bootstrap para cabecera menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Primera APP Angular';
+  title = 'Welcome to';
   autor = 'Alain';
 }
