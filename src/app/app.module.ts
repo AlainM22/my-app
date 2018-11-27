5af7044692c45cb8d08fa235f6e310b1bd72a96e
Curso <Curso@IPARTEKAULA.COM>
2018-11-27 14:26:02 +0100

    Componentes en la carpeta components y maquetacion page404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FrutaComponent } from './fruta/fruta.component';
-import { AlumnoComponent } from './alumno/alumno.component';
-import { FlujoInformacionComponent } from './flujo-informacion/flujo-informacion.component';
+import { FrutaComponent } from './components/fruta/fruta.component';
+import { AlumnoComponent } from './components/alumno/alumno.component';
+import { FlujoInformacionComponent } from './components/flujo-informacion/flujo-informacion.component';
 import { HomeComponent } from './components/home/home.component';
 import { Page404Component } from './components/page404/page404.component';
 
